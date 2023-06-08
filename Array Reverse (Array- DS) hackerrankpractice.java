073d90ac3aebd659db66b77d82be504b234ad37f
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2023-06-09 03:10:08 +0530

    Array Reverse (Array- DS) hackerrankpractice JAVA
    100 Days of Coding In Java on Hacker Rank - Array- DS Solution

---
+++ Array Reverse (Array- DS) hackerrankpractice.java
@@ -0,0 +1,34 @@
+// Challenge link : https://www.hackerrank.com/challenges/arrays-ds/problem?isFullScreen=true
+
+// Git Hub REPO Link :  https://github.com/piyushpatelcodes/100-Days-Java-Hackerrank
+
+import java.util.*;
+
+public class hackerrankpractice{
+    public static int arrayreverse(int arr[]){
+
+        System.out.print("reversed array: ");
+        for(int i = arr.length - 1; i>=0; i--){
+            System.out.print(arr[i] + " ");
+        }
+
+        
+    return 0;} 
+
+
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array range");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array ::");  
+  
+        for(int i=0; i<size; i++) {
+            arr[i] = sc.nextInt();
+        }  
+
+        arrayreverse(arr);
+
+        }
+}
