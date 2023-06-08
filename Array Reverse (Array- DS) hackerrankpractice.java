8fb622475978b5682ebf4e6fa754e32643f88fa7
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2023-06-09 03:11:26 +0530

    Array Reverse (Array- DS) hackerrankpractice.java

--- Array Reverse (Array- DS) hackerrankpractice.java
+++ Array Reverse (Array- DS) hackerrankpractice.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class hackerrankpractice{
     public static int arrayreverse(int arr[]){
 
-        System.out.print("reversed array: ");
+//         System.out.print("reversed array: ");
         for(int i = arr.length - 1; i>=0; i--){
             System.out.print(arr[i] + " ");
         }
@@ -19,10 +19,10 @@ public class hackerrankpractice{
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the array range");
+//         System.out.println("Enter the array range");
         int size = sc.nextInt();
         int[] arr = new int[size];
-        System.out.println("Enter the elements of the array ::");  
+//         System.out.println("Enter the elements of the array ::");  
   
         for(int i=0; i<size; i++) {
             arr[i] = sc.nextInt();
