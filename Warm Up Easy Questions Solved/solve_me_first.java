7f386656dc2a6c8be37e8f47c18f3dd1754c8aea
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2023-06-13 23:53:16 +0530

    Add files via upload

---
+++ Warm Up Easy Questions Solved/solve_me_first.java
@@ -0,0 +1,26 @@
+// Challenge link : https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+// Git Hub REPO Link :  https://github.com/piyushpatelcodes/100-Days-Java-Hackerrank
+
+import java.io.*;
+import java.util.*;
+
+public class solve_me_first{
+    public static int sum(int a, int b){
+        System.out.println(a+b);
+    return 0;}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // System.out.println("a = ");
+        int a = sc.nextInt();
+        
+        // System.out.println("b = ");
+        int b = sc.nextInt();
+        
+        sum(a,b);
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }  
+}
